805a02bc424192e11150c2f36630f5befe95bce5
Tekubambam <65901088+Em-Verdier@users.noreply.github.com>
2020-10-20 09:41:23 +0200

    Update calc.sol

--- calc.sol
+++ calc.sol
@@ -1,38 +1,53 @@
-// SPDX-License-Identifier: MIT                 // SPDX Licence Identifier
-pragma solidity ^0.7.0; // Version pragma
-pragma experimental ABIEncoderV2; // Experimental pragma
+// SPDX-License-Identifier: MIT                 r
+pragma solidity >=0.6.0;
+pragma experimental ABIEncoderV2;
 
+/// @title A blockchain calculator API
+/// @author Sofiane Akermoun <absolutevirtuexi@gmail.com>
+/// @notice This contract was written for the dwd studens from Alyra School
+/// @dev All function calls are currently implemented without side effects
 contract Calc {
+  address private owner;
 
-      address owner;
+  constructor() public {
+    owner = msg.sender;
+  }
 
-      constructor() public{
-      owner = msg.sender
-}
-      modifier _ownerOnly(){
-      require(msg.sender == owner);
-}
-    uint256 c;
+  modifier onlyOwner() {
+    require(owner == msg.sender, 'Only owner can use this function');
+    _;
+  }
 
-    function add(uint256 a, uint256 b) public {
-        c = a + b;
-    }
+  /// @notice An addition function
+  /// @param a First number of the add operation
+  /// @param b Second number of the add operation
+  /// @return the result of the addition a + b
+  function add(uint256 a, uint256 b) public view onlyOwner returns (uint256) {
+    return a + b;
+  }
 
-    function sub(uint256 a, uint256 b) public {
-        c = a - b;
-    }
+  /// @notice A substraction function
+  /// @param a First number of the sub operation
+  /// @param b Second number of the sub operation
+  /// @return the result of the substraction a - b
+  function sub(uint256 a, uint256 b) public view onlyOwner returns (uint256) {
+    return a - b;
+  }
 
-    function mul(uint256 a, uint256 b) public {
-        c = a * b;
-    }
+  /// @notice A multiplication function
+  /// @param a First number of the sub operation
+  /// @param b Second number of the sub operation
+  /// @return the result of the multiplication a * b
+  function mul(uint256 a, uint256 b) public view onlyOwner returns (uint256) {
+    return a * b;
+  }
 
-    function div(uint256 a, uint256 b) public {
-        require(b > 0, "Second argument must be different than 0");
-
-        c = a / b;
-    }
-
-    function getResult() public view returns (uint256 x) {
-        return c;
-    }
+  /// @notice A division function
+  /// @dev need to check for 0 division
+  /// @param a The numerator
+  /// @param b The denominator
+  /// @return the result of the division a / b
+  function div(uint256 a, uint256 b) public view onlyOwner returns (uint256) {
+    return a / b;
+  }
 }
